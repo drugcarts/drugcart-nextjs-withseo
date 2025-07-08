4a99afdebf52d8e5a53e61311781362f7cb1e57e
joseferrar <joseferrar4@gmail.com>
2025-07-08 16:00:17 +0530

    category icon delay issue fixed

--- src/components/home/TopCategoryClient.jsx
+++ src/components/home/TopCategoryClient.jsx
@@ -1,36 +1,71 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { IMAGES } from "../common/images";
+import Image from "next/image";
 
 const TopCategoryClient = ({ categories }) => {
   const router = useRouter();
 
-  const CategoryImage = ({ cat_img, category_name }) => {
-    const newUrl = `https://drugcarts-nextjs.s3.ap-south-1.amazonaws.com/category/thumb/${cat_img}`;
-    const oldUrl = `https://assets2.drugcarts.com/category/thumb/${cat_img}`;
-    const noImage = "./noimage.png";
+  // const CategoryImage = ({ cat_img, category_name }) => {
+  //   const newUrl = `https://drugcarts-nextjs.s3.ap-south-1.amazonaws.com/category/thumb/${cat_img}`;
+  //   const oldUrl = `https://assets2.drugcarts.com/category/thumb/${cat_img}`;
+  //   const noImage = "./noimage.png";
 
-    const [src, setSrc] = useState(newUrl);
+  //   const [src, setSrc] = useState(newUrl);
+
+  //   const handleError = () => {
+  //     if (src !== oldUrl) {
+  //       setSrc(oldUrl); // Fallback to old image
+  //     } else {
+  //       setSrc(noImage);
+  //     }
+  //   };
+
+  //   return (
+  //     <img
+  //       src={src}
+  //       onError={handleError}
+  //       width={100}
+  //       height={100}
+  //       alt={category_name}
+  //       className="mb-3 mx-auto object-cover bg-bgcancer rounded-full p-2 w-24 h-24"
+  //     />
+  //   );
+  // };
+
+  const CategoryImage = ({ categories }) => {
+    const primaryImage = categories?.cat_img
+      ? `https://assets2.drugcarts.com/category/thumb/${categories.cat_img}`
+      : null;
+
+    const fallbackImage = categories?.cat_img
+      ? `https://drugcarts-nextjs.s3.ap-south-1.amazonaws.com/category/thumb/${categories.cat_img}`
+      : null;
+
+    const [imgSrc, setImgSrc] = useState(primaryImage || IMAGES.NO_IMAGE);
 
     const handleError = () => {
-      if (src !== oldUrl) {
-        setSrc(oldUrl); // Fallback to old image
+      if (imgSrc !== fallbackImage && fallbackImage) {
+        setImgSrc(fallbackImage);
       } else {
-        setSrc(noImage);
+        setImgSrc(IMAGES.NO_IMAGE);
       }
     };
 
     return (
-      <img
-        src={src}
-        onError={handleError}
+      <Image
+        priority
+        src={imgSrc}
+        alt={categories?.category_name || "Category Image"}
         width={100}
         height={100}
-        alt={category_name}
         className="mb-3 mx-auto object-cover bg-bgcancer rounded-full p-2 w-24 h-24"
+        onError={handleError}
       />
     );
   };
+
   return (
     <>
       {categories?.map((category, i) => {
@@ -42,8 +77,7 @@ const TopCategoryClient = ({ categories }) => {
           >
             <p className="text-center">
               <CategoryImage
-                cat_img={category?.cat_img}
-                category_name={category?.category_name}
+                categories={category}
               />
               <span>{category?.category_name}</span>
             </p>
